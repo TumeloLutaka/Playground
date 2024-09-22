8200dbac3aa0a7d76734582276d9515f66407947
TumeloLutaka <tumelolutakacode@gmail.com>
2024-09-22 20:38:39 +0200

    Finishing up Gym App and updating playground page

--- Websites/Smoljames/river-fit/src/components/Hero.jsx
+++ Websites/Smoljames/river-fit/src/components/Hero.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import Button from "./Button";
 
 export default function Hero() {
   return (
-    <div className="min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto">
+    <div className="min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4">
       <div className="flex flex-col gap-4">
         <p>IT'S TIME TO GET</p>
         <h1 className="uppercase font-semibold text-4xl sm:text-5xl md:6xl lg:text-7xl">
@@ -11,17 +12,20 @@ export default function Hero() {
       </div>
 
       <p className="text-sm md:text-base font-light">
-        I hereby acknowledge that I may become{" "}
+        I hereby acknowledge that I may become
         <span className="text-blue-400 font-medium">
           unbelieveable enormous
-        </span>{" "}
-        and accept all risk of becoming the local{" "}
+        </span>
+        and accept all risk of becoming the local
         <span className="text-blue-400 font-medium">mass monstrosity</span>,
         afflicted with severe body dismorphia, unable to fit through doors
       </p>
-      <button className="px-8 py-4 rounded-medium border-[2px] bg-slate-950 border-blue-400 border-solid blue-shadow">
-        <p>Accept & Begin</p>
-      </button>
+      <Button
+        func={() => {
+          window.location.href = "#generate";
+        }}
+        text={"Accept & Begin"}
+      />
     </div>
   );
 }
