67bcdd60676272a0c966826886f8c0c87d4c4ddb
TumeloLutaka <tumelolutakacode@gmail.com>
2024-09-25 22:37:05 +0200

    Inital FreeScribe Commit

--- Websites/Smoljames/free-scribe/src/App.jsx
+++ Websites/Smoljames/free-scribe/src/App.jsx
@@ -1,7 +1,46 @@
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import HomePage from "./components/HomePage";
+import FileDisplay from "./components/FileDisplay";
+import Information from "./components/Information";
+import Transcribing from "./components/Transcribing";
+
 function App() {
+  const [file, setFile] = useState(null);
+  const [audioStream, setAudioStream] = useState(null);
+  const [output, setOutput] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const isAudioAvaliable = file || audioStream;
+
+  function handleAudioReset() {
+    setFile(null);
+    setAudioStream(null);
+  }
+
+  useEffect(() => {
+    console.log(audioStream);
+  }, [audioStream]);
+
   return (
-    <div>
-      <h1 className="text-green-400">Green</h1>
+    <div className="fex flex-col max-w-[1000px] mx-auto w-full">
+      <section className="min-h-screen flex flex-col">
+        <Header />
+        {output ? (
+          <Information />
+        ) : loading ? (
+          <Transcribing />
+        ) : isAudioAvaliable ? (
+          <FileDisplay
+            file={file}
+            audioStream={audioStream}
+            handleAudioReset={handleAudioReset}
+          />
+        ) : (
+          <HomePage setFile={setFile} setAudioStream={setAudioStream} />
+        )}
+      </section>
+      <footer></footer>
     </div>
   );
 }
---
+++ Websites/Smoljames/free-scribe/src/components/FileDisplay.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function FileDisplay(props) {
+  const { file, audioStream, handleAudioReset } = props;
+
+  return (
+    <main className="flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4 justify-center pb-20 min-w-72 sm:w-96 max-w-full mx-auto">
+      <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl">
+        Your <span className="text-blue-400 bold">File</span>
+      </h1>
+      <div className="flex flex-col text-left mb-4 my-4">
+        <h3 className="font-semibold">Name </h3>
+        <p>{file ? file.name : "Custom Audio"}</p>
+      </div>
+      <div className="flex items-center justify-between gap-4">
+        <button
+          onClick={handleAudioReset}
+          className="text-slate-400 hover:text-blue-600 duration-200"
+        >
+          Reset
+        </button>
+        <button className="specialBtn px-3 p-2 rounded-lg text-blue-400">
+          <p className="flex items-center gap-2 font-medium">Transcribe</p>
+          <i className="fa-solid fa-pen-nib"></i>
+        </button>
+      </div>
+    </main>
+  );
+}
