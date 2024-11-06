e823a6b6296d082b10dfba38c1fe190bbf7df4c1
TumeloLutaka <126949443+TumeloLutaka@users.noreply.github.com>
2024-11-06 13:14:54 +0000

    Reworked plaground homescreen

---
+++ src/Pages/Home/HomeScreen/index.jsx
@@ -0,0 +1,17 @@
+import HeroSection from "../HeroSection";
+import MySkills from "../MySkills";
+import MyPortfolio from "../MyPortfolio";
+import ContactMe from "../ContactMe";
+import Footer from "../Footer";
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <MySkills />
+      <MyPortfolio />
+      <ContactMe />
+      <Footer />
+    </>
+  );
+}
